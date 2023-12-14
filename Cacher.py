f5f9165ce739062bd8480c73a5b6e12b804b514c
Kenan3M5 <151612322+Kenan3M5@users.noreply.github.com>
2023-12-14 16:24:06 +0100

    Update Cacher.py

--- Cacher.py
+++ Cacher.py
@@ -2,7 +2,7 @@
 
 from PIL import Image
 
-image_to_change = "image/to/change.jpg"
+image_to_change = "lapin.jpeg"
 img = Image.open(image_to_change)
 
 #print("Image format:", img.format)
@@ -11,5 +11,31 @@ img = Image.open(image_to_change)
 # pixel = image.getpixel((x, y))
 #image.putpixel((x, y), new_color)
 
+largeur, hauteur = img.size
+
+def changeRGB(x,y):
+    
+    """
+    Cette fonction prend la valeur R d'un pixel et la rend pair.
+    """
+    red, green, blue = img.getpixel((x,y))
+    if red == 255:
+        red = red - 1
+    else:
+        if red%2 == 1:
+            red = red + 1
+    img.putpixel((x, y), (red,green, blue))      
+
+for x in range(largeur):
+    for y in range(hauteur):
+        changeRGB(x,y)
+        print(img.getpixel((x,y)))
+
 img.show()
 
+valeur_numerique_des_lettres_du_message = []
+
+message = "Farouk aura 18,5 l'année prochaine"
+
+for i in range(len(message)):
+    ord(i)
