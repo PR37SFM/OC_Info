7ca9fbe3844f2c74e7c7c5d1a33bdcbf7f34dd4c
Kenan3M5 <151612322+Kenan3M5@users.noreply.github.com>
2023-12-17 19:14:48 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print(chr(65))
+import os
+
+# Get the current working directory
+current_directory = os.getcwd()
+
+# Combine the current directory with the image filename
+image_filename = "image_changed.jpeg"
+image_path = os.path.join(current_directory, image_filename)
+
+print("Path to the image:", image_path)
