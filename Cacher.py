a6f20285fd57881d660e3bffa1c675f3001bd193
Kenan3M5 <151612322+Kenan3M5@users.noreply.github.com>
2023-12-17 19:16:22 +0100

    Update Cacher.py

--- Cacher.py
+++ Cacher.py
@@ -24,16 +24,13 @@ def changeRGB(img, x,y):
 def numeriser_message(message):
     """
     1 arg (string) -> list of int
-    
     Cette fonction reçoit un message et renvoie une liste.
     Chaque élément de la liste est la valeur numérique en ascii d'un caractère du message.
     Les éléments de la liste sont dans le même ordre que les caractères du message.
-    
     """
     message_en_ascii = []
     for i in range(len(message)):
         message_en_ascii.append(ord(message[i]))
-        
     return message_en_ascii
 
 
@@ -42,19 +39,14 @@ def numeriser_message(message):
 def liste_coordonnées_alea (hauteur, n_coord_aléa):
     """
     input:  1 int -> output: 1 new_liste
-    
     Cette fonction reçoit un  nombre x.
     Elle renvoie une nouvelle liste de x éléments différents de la liste liste_des_valeurs_y_possible de façon aléatoire
     """
-
     liste_des_valeurs_y_possible = []
     for i in range(1, hauteur):
         liste_des_valeurs_y_possible.append(i)
-
     coordonnées_alea = random.sample(liste_des_valeurs_y_possible, n_coord_aléa)
     return coordonnées_alea
-
-
 
 
 
@@ -62,13 +54,10 @@ def liste_coordonnées_alea (hauteur, n_coord_aléa):
 def cache_nombre_sur_une_colonne(img,x):
     """
     1 int -> return None
-    
     cette Fonction reçoit un nombre x.
-    
     Elle reçoit 
     """
-    largeur, hauteur = img.size
-            
+    largeur, hauteur = img.size    
     for i in liste_coordonnées_alea(hauteur, numeriser_message(message)[x]):
         red, green, blue = img.getpixel((x,i))
         red = red + 1
@@ -76,26 +65,18 @@ def cache_nombre_sur_une_colonne(img,x):
 
 
 
-def compter_impair_colonne (img, x):
-    
-    largeur, hauteur = img.size
-    
-    n_red_impair = 0
-    for i in range(hauteur):
-        red, green, blue = img.getpixel((x,i))
-        if red%2 == 1:
-            n_red_impair = n_red_impair + 1
-    return n_red_impair
-
 
 def cacher_message(image_to_change,message):
 
-
-
+    """
+    input: (string,string)
+    
+    Cette fonction demande comme premier argument le nom du fichier jpeg de l'image dans laquelle l'utilisateur veut cacher un message.
+    Puis comme 2ème argument le message que l'utilisateur veut cacher.
+    """
     img = Image.open(image_to_change)
-
     largeur, hauteur = img.size
-
+    
     for x in range(largeur):
         for y in range(hauteur):
             changeRGB(img,x,y)
@@ -103,18 +84,12 @@ def cacher_message(image_to_change,message):
     for x in range(len(message)):
         cache_nombre_sur_une_colonne(img,x)
     
-    
-    
-    for x in range(len(message)):
-        print(chr(compter_impair_colonne(img,x)))
-
     img.save("image_changed.jpeg")
 
-
-    
-
-    
     img.close()
+    
+
+
         
 message = input("Ecris-moi le message que tu veux caché dans l'image:")
 
@@ -126,6 +101,10 @@ cacher_message(image_to_change,message)
 
 
 
+"""
+
+Ici, on a la version de trouver le message qui marche mais seulement ici.
+
 
 
 def compter_impair_colonne (img, x):
@@ -141,42 +120,7 @@ def compter_impair_colonne (img, x):
 
 
 
-
-
-"""    
-def cache_nombre_sur_une_colonne(i):
-    
-    for x in range(largeur):
-        for y in range(hauteur):
-            changeRGB(x,y)
-    
-
-    valeurs_y = choisir_coordonnées_alea(numeriser_message(message)[i])
-    
-    for z in valeurs_y:
-        red, green, blue = img.getpixel((i,z))
-        red = red + 1
-        img.putpixel((i, valeurs_y[z]), (red,green, blue))
-        
-    return red
-        
-print(cache_nombre_sur_une_colonne(0))
-
-
-
-        
-img.save("image_changed.jpeg")
-
-img.show()
-        
-"""
-"""
-    reçoit liste d'int
-    pour le int de valeur x à la n-ième position, prendre coordonée (n,y) de l'image.
-    prendre aléatoirement x éléments de cette liste (a,b,c,...)
-    changer le R en faisant + 1 (ou -1 si max) de la position (x,a),(x,b),...)
-    changer 
+for x in range(len(message)):
+    print(chr(compter_impair_colonne(img,x)))
     
     """
-        
-
