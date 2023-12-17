8d0207b0d245fdf3b1716b1f78d17f658e38e69f
Kenan3M5 <151612322+Kenan3M5@users.noreply.github.com>
2023-12-17 19:14:34 +0100

    Update Cacher.py

--- Cacher.py
+++ Cacher.py
@@ -2,24 +2,10 @@ from PIL import Image
 
 import random
 
-### À changer pour input
-
-image_to_change = "image.jpeg"
-
-### À changer pour input
 
 
-img = Image.open(image_to_change)
 
-#print("Image format:", img.format)
-#print("Image mode:", img.mode)
-#print("Image size:", img.size)
-# pixel = image.getpixel((x, y))
-#image.putpixel((x, y), new_color)
-
-largeur, hauteur = img.size
-
-def changeRGB(x,y):
+def changeRGB(img, x,y):
     
     """
     Cette fonction prend la valeur R d'un pixel et la rend pair.
@@ -35,16 +21,7 @@ def changeRGB(x,y):
 
 
 
-### À changer pour input
-
-message = "(Farouk aura 18,5 ans l'année prochaine.)"
-
-### À changer pour input
-
-message_en_ascii = []
-
 def numeriser_message(message):
-    
     """
     1 arg (string) -> list of int
     
@@ -53,7 +30,7 @@ def numeriser_message(message):
     Les éléments de la liste sont dans le même ordre que les caractères du message.
     
     """
-    
+    message_en_ascii = []
     for i in range(len(message)):
         message_en_ascii.append(ord(message[i]))
         
@@ -62,13 +39,14 @@ def numeriser_message(message):
 
         
     
-def choisir_coordonnées_alea (n_coord_aléa):
+def liste_coordonnées_alea (hauteur, n_coord_aléa):
     """
     input:  1 int -> output: 1 new_liste
     
-    Cette fonction reçoit un un nombre x.
+    Cette fonction reçoit un  nombre x.
     Elle renvoie une nouvelle liste de x éléments différents de la liste liste_des_valeurs_y_possible de façon aléatoire
     """
+
     liste_des_valeurs_y_possible = []
     for i in range(1, hauteur):
         liste_des_valeurs_y_possible.append(i)
@@ -78,27 +56,30 @@ def choisir_coordonnées_alea (n_coord_aléa):
 
 
 
-for x in range(largeur):
-    for y in range(hauteur):
-        changeRGB(x,y)  
 
 
-def cache_nombre_sur_une_colonne(i):
-    for y in choisir_coordonnées_alea (numeriser_message(message)[i]):
-        red, green, blue = img.getpixel((i,y))
+
+def cache_nombre_sur_une_colonne(img,x):
+    """
+    1 int -> return None
+    
+    cette Fonction reçoit un nombre x.
+    
+    Elle reçoit 
+    """
+    largeur, hauteur = img.size
+            
+    for i in liste_coordonnées_alea(hauteur, numeriser_message(message)[x]):
+        red, green, blue = img.getpixel((x,i))
         red = red + 1
-        img.putpixel((i,y),(red, green, blue))
-
-for x in range(len(message)):
-    cache_nombre_sur_une_colonne(x)
+        img.putpixel((x,i),(red, green, blue))
 
 
 
-
-
-img.save("image_changed.jpeg")
-
-def compter_impair_colonne (x):
+def compter_impair_colonne (img, x):
+    
+    largeur, hauteur = img.size
+    
     n_red_impair = 0
     for i in range(hauteur):
         red, green, blue = img.getpixel((x,i))
@@ -107,8 +88,58 @@ def compter_impair_colonne (x):
     return n_red_impair
 
 
-for x in range(len(message)):
-   print(chr(compter_impair_colonne(x)))
+def cacher_message(image_to_change,message):
+
+
+
+    img = Image.open(image_to_change)
+
+    largeur, hauteur = img.size
+
+    for x in range(largeur):
+        for y in range(hauteur):
+            changeRGB(img,x,y)
+
+    for x in range(len(message)):
+        cache_nombre_sur_une_colonne(img,x)
+    
+    
+    
+    for x in range(len(message)):
+        print(chr(compter_impair_colonne(img,x)))
+
+    img.save("image_changed.jpeg")
+
+
+    
+
+    
+    img.close()
+        
+message = input("Ecris-moi le message que tu veux caché dans l'image:")
+
+image_to_change = input("Ecris-moi le nom du fichier jpeg de l'image où tu veux cacher un message:")
+
+cacher_message(image_to_change,message)
+
+
+
+
+
+
+
+def compter_impair_colonne (img, x):
+    
+    largeur, hauteur = img.size
+    
+    n_red_impair = 0
+    for i in range(hauteur):
+        red, green, blue = img.getpixel((x,i))
+        if red%2 == 1:
+            n_red_impair = n_red_impair + 1
+    return n_red_impair
+
+
 
 
 
