d7c5dc57e8a8da8ca0632eb783d8b7db3cd0472a
Kenan3M5 <151612322+Kenan3M5@users.noreply.github.com>
2023-12-24 10:50:33 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,57 @@
-import os
+from PIL import Image
 
-# Get the current working directory
-current_directory = os.getcwd()
+liste_de_chaines = []
 
-# Combine the current directory with the image filename
-image_filename = "image_changed.jpeg"
-image_path = os.path.join(current_directory, image_filename)
+# Ajouter une chaîne de caractères initiale à la liste
+chaine_initiale = "Bonjour"
+liste_de_chaines.append(chaine_initiale)
 
-print("Path to the image:", image_path)
+# Ajouter des lettres à la chaîne de caractères
+lettre_a_ajouter = "X"
+chaine_modifiee = chaine_initiale + lettre_a_ajouter
+liste_de_chaines.append(chaine_modifiee)
+
+# Vous pouvez également ajouter des lettres directement à la dernière chaîne ajoutée
+lettre_suivante = "Y"
+liste_de_chaines[-1] += lettre_suivante
+
+# Afficher le résultat
+print(liste_de_chaines)
+
+
+
+"""
+from PIL import Image
+
+
+def changeRGB(img:Image, x,y):
+    
+    
+    Cette fonction prend la valeur R d'un pixel et la rend pair.
+    
+    color = img.getpixel((x, y))
+    print(color[0])
+    if color[0] == 255:
+        color = (color[0] - 1, color[1], color[2])
+    else:
+        if color[0] % 2 == 1:
+            color = (color[0] + 1, color[1], color[2])
+    
+    img.putpixel((x, y), color)
+    
+    
+
+img =  img = Image.open("image.png")
+
+largeur, hauteur = img.size
+
+print(img.getpixel((600,600)))
+help(Image.getpixel())
+
+
+for x in range(largeur):
+    for y in range(hauteur):
+        changeRGB(img,x,y)
+        print(img.getpixel((x,y)))
+        
+"""
