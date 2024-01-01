6e23f78d8bf5441b00b74aafbc8d2c81b8cc51d4
Kenan3M5 <151612322+Kenan3M5@users.noreply.github.com>
2024-01-01 12:58:08 +0100

    Update retrouver.py

--- retrouver.py
+++ retrouver.py
@@ -1,21 +1,12 @@
 from PIL import Image
 
 
-### À changer pour input
-
-image_to_solve = input("Donne moi le nom du fichier qui contient le message caché: ")
 
 
 
-img = Image.open(image_to_solve)
-
-largeur, hauteur = img.size
-
-
-
-
-def compter_impair_colonne (x):
+def compter_impair_colonne (x,img):
     n_red_impair = 0
+    largeur, hauteur = img.size
     for i in range(hauteur):
         red, green, blue,alpha = img.getpixel((x,i))
         if red%2 == 1:
@@ -23,10 +14,16 @@ def compter_impair_colonne (x):
     return n_red_impair
 
 
-message = ""
+def decoder_message(image_to_solve):
+    message = ""
+    img = Image.open(image_to_solve)
+    largeur, hauteur = img.size
+    for x in range(largeur):
+        message += chr(compter_impair_colonne(x,img))
+    return message
 
-for x in range(largeur):
-    message += chr(compter_impair_colonne(x))
-    
-print(message)
 
+ 
+image_to_solve = input("Donne moi le nom du fichier qui contient le message caché: ")
+
+print(decoder_message(image_to_solve))
