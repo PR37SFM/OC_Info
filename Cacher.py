c917835dca5bd33067ae1a8a60955e5c1401ddee
Kenan3M5 <151612322+Kenan3M5@users.noreply.github.com>
2024-01-11 12:40:19 +0100

    Update Cacher.py

--- Cacher.py
+++ Cacher.py
@@ -151,16 +151,7 @@ def cacher_message(image_to_change,message,image_changed):
     
 
 """
-The path of the righteous man is beset on all sides
-By the inequities of the selfish and the tyranny of evil men
-Blessed is he who, in the name of charity and good will
-Shepherds the weak through the valley of darkness
-For he is truly his brother's keeper and the finder of lost children
-And I will strike down upon thee
-With great vengeance and furious anger
-Those who attempt to poison and destroy my brothers
-And you will know my name is the Lord
-When I lay my vengeance upon thee
+The path of the righteous man is beset on all sides By the inequities of the selfish and the tyranny of evil men Blessed is he who, in the name of charity and good will Shepherds the weak through the valley of darkness For he is truly his brother's keeper and the finder of lost children And I will strike down upon thee With great vengeance and furious anger Those who attempt to poison and destroy my brothers And you will know my name is the Lord When I lay my vengeance upon thee
 """
 
 
