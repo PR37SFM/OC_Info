0231935adf38365a7723aa227aff185a4dd4fff1
Kenan3M5 <151612322+Kenan3M5@users.noreply.github.com>
2024-01-11 12:28:56 +0100

    Update decrypt_03.py

--- Méthode 2/decrypt_03.py
+++ Méthode 2/decrypt_03.py
@@ -1,8 +1,8 @@
 from PIL import Image
 from module_03 import input_filename, decrypt_image
 
-#filename = input_filename("Entrez l'image :")
-filename = './nature_modified.png'
+filename = input_filename("Entrez l'image :")
+#filename = './nature_modified.png'
 image = Image.open(filename)
 message = decrypt_image(image)
 if message == '':
